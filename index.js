642e1c2631fd1c16818bcddece54ec71b5c7b556
Justin Chase <justin@evovlehq.com>
2014-12-01 17:06:35 -0600

    Added the append function.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 require('shelljs/global');
+var path = require('path');
 
-module.exports = seven_zip;
+var sevenZipPath = path.join(__dirname, 'bin', 'win32', '7z');
 
-function seven_zip() {
+var sevenZip = {};
+module['exports'] = sevenZip;
 
+function append() {
+	var outFile = arguments[0];
+	var infiles = [];
+	for(var i = 1; i < arguments.length; i++) {
+		infiles.push(arguments[i]);
+	}
+
+	var cmd = sevenZipPath + " a " + outFile + " " + infiles.join(" ");
+	return exec(cmd);
 }
+
+sevenZip.append = append;
